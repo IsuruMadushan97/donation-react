d919e52396238c7e91bb6b4e9bd676a329ba6c82
ialmoqren <ialmoqren@gmail.com>
2018-06-07 12:31:41 +0300

    Hospitals can edit there account

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,9 @@ import "./style/app.css";
 
 import donorRegister from "./donorComponents/donorRegister";
 import loggedInDonor from "./donorComponents/loggedInDonor";
-import editDonorAcct from "./donorComponents/editDonorAcct";
 
 import hospitalRegister from "./hospitalComponents/hospitalRegister";
 import loggedInHospital from "./hospitalComponents/loggedInHospital";
-import editHospitalAcct from "./hospitalComponents/editHospitalAcct";
 
 // import route Components here
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
@@ -20,11 +18,9 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={donorRegister} />
             <Route path="/loggedInDonor" component={loggedInDonor} />
-            <Route path="/editDonorAcct" component={editDonorAcct} />
 
             <Route path="/hospitalRegister" component={hospitalRegister} />
             <Route path="/loggedInHospital" component={loggedInHospital} />
-            <Route path="/editHospitalAcct" component={editHospitalAcct} />
           </Switch>
         </div>
       </Router>
--- my-app/src/hospitalComponents/loggedInHospital.js
+++ my-app/src/hospitalComponents/loggedInHospital.js
@@ -1,31 +1,86 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+let em = 1;
+let emil = "asd";
 
 class loggedInHospital extends React.Component {
   state = {
     donations: [],
-    donors: []
+    donors: [],
+    hospitalFullName: "",
+    hospitalEmail: "",
+    hospitalCity: "",
+    hospitalPassword: ""
   };
   componentDidMount() {
     let donations = [];
     axios.get("http://localhost:8000/donations").then(res => {
       donations = res.data;
-      console.log(donations);
 
       let donors = [];
       donations.forEach(element => {
-        // console.log(element.donor_email);
-
-        let getString = "http://localhost:8000/donors/" + element.donor_email;
+        let getString = "http://localhost:8000/donors/" + element.donor_id;
         axios.get(getString).then(res => {
           let donor = res.data;
           // this.setState({ donations });
           donors.push(donor);
         });
       });
+
       this.setState({ donations: donations, donors: donors });
-      console.log(this.state.donors);
+    });
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  onSubmitUpdateHospital = e => {
+    e.preventDefault();
+    let hospitalFullName = this.state.hospitalFullName;
+    let hospitalEmail = this.state.hospitalEmail;
+    let hospitalCity = this.state.hospitalCity;
+    let hospitalPassword = this.state.hospitalPassword;
+    // hospitalFullName = encodeURIComponent(hospitalFullName);
+    // hospitalCity = encodeURIComponent(hospitalCity);
+    // hospitalPassword = encodeURIComponent(hospitalPassword);
+
+    //    http://localhost:8000/hospitals/3?email=SSS&name=SSS&city=SSS&password=SSS
+    var postString =
+      "http://localhost:8000/hospitals/" +
+      em +
+      "?email=" +
+      hospitalEmail +
+      "&name=" +
+      hospitalFullName +
+      "&city=" +
+      hospitalCity +
+      "&password=" +
+      hospitalPassword;
+    axios.post(postString, {}).then(result => {});
+    setTimeout(() => {
+      this.componentDidMount();
+    }, 500);
+    this.cancel();
+  };
+
+  editAcct(e) {
+    let body1 = document.getElementById("body3");
+    let body2 = document.getElementById("body4");
+    body1.style = "display:none";
+    body2.style = "display:block";
+
+    let getString = "http://localhost:8000/hospitals/" + emil;
+    axios.get(getString).then(res => {
+      const hospitalInfo = res.data;
+      // this.setState({ donations: donations });
+      this.setState({
+        hospitalFullName: hospitalInfo.name,
+        hospitalEmail: hospitalInfo.email,
+        hospitalCity: hospitalInfo.city,
+        hospitalPassword: hospitalInfo.password
+      });
     });
   }
 
@@ -53,101 +108,175 @@ class loggedInHospital extends React.Component {
     }
   }
 
+  cancel(e) {
+    let body1 = document.getElementById("body3");
+    let body2 = document.getElementById("body4");
+    body1.style = "display:block";
+    body2.style = "display:none";
+  }
+
   render() {
     return (
       <div>
-        <header>Hospitals Site</header>
-        <div id="fullBox3">
-          <Link to="/editHospitalAcct" className={"text-danger"}>
-            Edit my account
-          </Link>
-          <Link to="/hospitalRegister" className={"text-dark"}>
-            Logout
-          </Link>
+        <div id="body3">
+          <header>Hospitals Site</header>
+          <div id="fullBox3">
+            <Link to="/hospitalRegister" className={"text-dark"}>
+              Logout
+            </Link>
 
-          <h2>Welcome!</h2>
-          <h5>Choose Donors:</h5>
-          <div id="searchBox">
+            <h2>Welcome!</h2>
             <input
-              type="text"
-              id="myInput"
-              className="form-control"
-              onKeyUp={this.search.bind(this)}
-              placeholder="Search.."
+              type="button"
+              value="Edit my account"
+              className="btn btn-light btn-block form-control"
+              name="cancel"
+              onClick={this.editAcct.bind(this)}
             />
-            <div id="options" className="form-group">
-              <select className="form-control" id="sel1">
-                <option value="0">ID</option>
-                <option value="1">Type</option>
-                <option value="2">Notes</option>
-                <option value="3">Date</option>
-                <option value="4">Gender</option>
-                <option value="5">Email</option>
-                <option value="6">Age</option>
-                <option value="7">City</option>
-              </select>
+
+            <h5>Choose Donors:</h5>
+            <div id="searchBox">
+              <input
+                type="text"
+                id="myInput"
+                className="form-control"
+                onKeyUp={this.search.bind(this)}
+                placeholder="Search.."
+              />
+              <div id="options" className="form-group">
+                <select className="form-control" id="sel1">
+                  <option value="0">ID</option>
+                  <option value="1">Type</option>
+                  <option value="2">Notes</option>
+                  <option value="3">Gender</option>
+                  <option value="4">Email</option>
+                  <option value="5">Age</option>
+                  <option value="6">City</option>
+                </select>
+              </div>
+            </div>
+            <div style={{ overflowX: "auto" }}>
+              <table id="myTable" className="table">
+                <thead className="thead-dark">
+                  <tr>
+                    <th>ID</th>
+                    <th>Type</th>
+                    <th>Notes</th>
+                    <th>Gender</th>
+                    <th>Email</th>
+                    <th>Age</th>
+                    <th>City</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.donations.map(donation => (
+                    <tr>
+                      <td>
+                        <b>{donation.id}</b>
+                      </td>
+                      <td>{donation.type}</td>
+                      <td>{donation.notes}</td>
+                      <td>{this.state.donors.gender}</td>
+                      <td>{this.state.donors.email}</td>
+                      <td>{this.state.donors.age}</td>
+                      <td>{this.state.donors.city}</td>
+                    </tr>
+                  ))}
+
+                  <tr>
+                    <td>
+                      <b>2</b>
+                    </td>
+                    <td>kidney</td>
+                    <td>none</td>
+                    <td>Female</td>
+                    <td>test@examlpe.com</td>
+                    <td>20</td>
+                    <td>Riyadh</td>
+                  </tr>
+                  <tr>
+                    <td>
+                      <b>3</b>
+                    </td>
+                    <td>lung</td>
+                    <td>none</td>
+                    <td>Male</td>
+                    <td>test@examlpe.com</td>
+                    <td>35</td>
+                    <td>Jeddah</td>
+                  </tr>
+                </tbody>
+              </table>
             </div>
           </div>
-          <div style={{ overflowX: "auto" }}>
-            <table id="myTable" className="table">
-              <thead className="thead-dark">
-                {/* <tr onclick="window.location='#';"> */}
-                <tr>
-                  {/* <th onclick="sortTable(0)" scope="col"> */}
-                  <th>ID</th>
-                  {/* <th onclick="sortTable(1)" scope="col"> */}
-                  <th>Type</th>
-                  {/* <th onclick="sortTable(2)" scope="col"> */}
-                  <th>Notes</th>
-                  {/* <th onclick="sortTable(3)" scope="col"> */}
-                  <th>Gender</th>
-                  {/* <th onclick="sortTable(4)" scope="col"> */}
-                  <th>Email</th>
-                  {/* <th onclick="sortTable(5)" scope="col"> */}
-                  <th>Age</th>
-                  {/* <th onclick="sortTable(6)" scope="col"> */}
-                  <th>City</th>
-                </tr>
-              </thead>
-              <tbody>
-                {this.state.donations.map(donation => (
-                  <tr>
-                    <th>{donation.id}</th>
-                    <td>{donation.type}</td>
-                    <td>{donation.notes}</td>
-                    {console.log("HERE1", this.state.donors)}
-                    {console.log("HERE2", this.state.donations)}
-                    <td>{this.state.donors.gender}</td>
-                    <td>{this.state.donors.email}</td>
-                    <td>{this.state.donors.age}</td>
-                    <td>{this.state.donors.city}</td>
-                  </tr>
-                ))}
-
-                <tr>
-                  <td>
-                    <b>2</b>
-                  </td>
-                  <td>kidney</td>
-                  <td>none</td>
-                  <td>Female</td>
-                  <td>test@examlpe.com</td>
-                  <td>20</td>
-                  <td>Riyadh</td>
-                </tr>
-                <tr>
-                  <td>
-                    <b>3</b>
-                  </td>
-                  <td>lung</td>
-                  <td>none</td>
-                  <td>Male</td>
-                  <td>test@examlpe.com</td>
-                  <td>35</td>
-                  <td>Jeddah</td>
-                </tr>
-              </tbody>
-            </table>
+        </div>
+        <div id="body4">
+          <header>Hospitals Site</header>
+          <div id="regBox2">
+            <h5>Edit My Account:</h5>
+            <form
+              className="form-signin"
+              onSubmit={this.onSubmitUpdateHospital}
+              id="hideform"
+            >
+              <div className="input-group">
+                <input
+                  type="text"
+                  name="hospitalFullName"
+                  value={this.state.hospitalFullName}
+                  className="form-control"
+                  placeholder="Hospital Full Name"
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="input-group">
+                <input
+                  type="text"
+                  name="hospitalEmail"
+                  value={this.state.hospitalEmail}
+                  className="form-control"
+                  placeholder="Admin Email"
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="input-group">
+                <input
+                  type="text"
+                  name="hospitalCity"
+                  value={this.state.hospitalCity}
+                  className="form-control"
+                  placeholder="City"
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="input-group">
+                <input
+                  type="text"
+                  name="hospitalPassword"
+                  value={this.state.hospitalPassword}
+                  className="form-control"
+                  placeholder="Password"
+                  onChange={this.onChange}
+                />
+              </div>
+              <div className="input-group">
+                <input
+                  type="submit"
+                  value="Confirm"
+                  className="btn btn-lg btn-light btn-block form-control"
+                  name="add"
+                />
+              </div>
+              <div className="input-group">
+                <input
+                  type="button"
+                  value="Cancel"
+                  className="btn btn-lg btn-light btn-block form-control"
+                  name="cancel"
+                  onClick={this.cancel.bind(this)}
+                />
+              </div>
+            </form>
           </div>
         </div>
       </div>
